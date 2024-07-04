7c705686db62a7e6bb70d25d20dee84667c9b300
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-07-04 21:39:18 +0530

    designed the side nav bar

--- src/components/templates/Sidenav.jsx
+++ src/components/templates/Sidenav.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sidenav = () => {
-  return <div className="w-[20%] h-full bg-red-100"></div>;
+  return (
+    <div className="w-[20%] h-full border-r-2 border-zinc-400 p-10">
+      <h1 className="text-2xl text-white font-bold">
+        <i class="text-[#6556CD] ri-tv-fill mr-3"></i>
+        <span className="text-2xl">MovieFlix</span>
+      </h1>
+      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
+        <h1 className="text-white font-semibold text-xl mt-10 mb-5">
+          New Feeds
+        </h1>
+
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-fire-fill"></i> Trending
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-bard-fill"></i> Popular
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-movie-2-fill"></i> Movies
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-tv-2-fill"></i> Tv Shows
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-team-fill"></i> People
+        </Link>
+      </nav>
+
+      <hr className="border-none h-[1px] bg-zinc-400" />
+
+      <nav className="flex flex-col text-zinc-400 text-xl gap-3">
+        <h1 className="text-white font-semibold text-xl mt-10 mb-5">
+          Website Information
+        </h1>
+
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-information-fill"></i> About
+        </Link>
+        <Link className="hover:bg-[#6556CD] hover:text-white rounded-lg duration-300 p-5">
+          <i class="ri-phone-fill"></i> Contact
+        </Link>
+      </nav>
+    </div>
+  );
 };
 
 export default Sidenav;
