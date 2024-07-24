e97c5adca8833b75adde7ab22ad8c6f26d1ab6b9
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-07-24 16:19:53 +0530

    completed people details component

--- src/components/PeopleDetails.jsx
+++ src/components/PeopleDetails.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { asyncLoadPeople } from "../store/actions/peopleAction";
 import Topnav from "./templates/Topnav";
+import Loading from "./Loading";
+import TrendingCards from "./templates/TrendingCards";
 
 const PeopleDetails = () => {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -11,7 +13,7 @@ const PeopleDetails = () => {
   const dispatch = useDispatch();
 
   const { info } = useSelector((state) => state.people);
-  console.log(info);
+  console.log(info.movieCredits.cast);
 
   useEffect(() => {
     dispatch(asyncLoadPeople(id));
@@ -40,11 +42,11 @@ const PeopleDetails = () => {
   return info ? (
     <div
       style={{
-        background: `linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.7), rgba(0,0,0,.8)), url(https://image.tmdb.org/t/p/original/${info.details.profile_path})`,
-        backgroundPosition: "top 5% left 50%",
+        background: `linear-gradient(rgba(0,0,0,.8), rgba(0,0,0,.8), rgba(0,0,0,.8)), url(https://image.tmdb.org/t/p/original/${info.details.profile_path})`,
+        backgroundPosition: "top 55% left 50%",
         backgroundSize: "cover",
       }}
-      className="relative w-screen h-[205vh] px-[10%]"
+      className="relative w-screen h-[170vh] px-[10%]"
     >
       {/* Search bar and logo */}
       <div
@@ -88,40 +90,16 @@ const PeopleDetails = () => {
         />
 
         <div className="content ml-[3%] text-white">
-          <h1 className="text-5xl font-black text-white ">
-            {info.details.name ||
-              info.details.title ||
-              info.details.original_name ||
-              info.details.original_title}
+          <div className="flex">
+            <h1 className="text-5xl font-black text-white ">
+              {info.details.name ||
+                info.details.title ||
+                info.details.original_name ||
+                info.details.original_title}
+            </h1>
 
-            {/* <small className="text-2xl font-bold text-zinc-300">
-              ({info.details.first_air_date.split("-")[0]})
-            </small> */}
-          </h1>
-
-          <div className="mt-3 mb-6 flex text-white items-center gap-x-1">
-            <i class="ri-star-fill text-[#E9C46A]"></i>{" "}
-            <h1>{Math.floor(info.details.popularity)}</h1>
-            <div className="text-[#E9C46A] flex gap-2">
-              <span className="text-xl ml-3">•</span>
-              <h1>DOB: {info.details.birthday}</h1>
-              {/* <span className="text-xl ml-3">•</span>
-              <h1>{info.details.genres.map((g) => g.name).join(", ")}</h1>
-              <span className="text-xl ml-3">•</span>
-              <h1>Seasons: {info.details.number_of_seasons}</h1>
-              <span className="text-xl ml-3">•</span>
-              <h1>Total Episodes: {info.details.number_of_episodes}</h1> */}
-            </div>
-          </div>
-
-          {/* <h1 className="text-2xl font-semibold italic text-zinc-200">
-            {info.details.tagline}
-          </h1> */}
-          <h1 className="mb-3 text-2xl mt-5">Biography</h1>
-          <p className="mb-1">{info.details.biography}</p>
-
-          <div className="flex items-center gap-24">
-            <div className="flex gap-8 mt-5">
+            {/* social Links   */}
+            <div className="flex gap-4 text-[34px] ml-6">
               {info.external_ids.facebook_id && (
                 <a
                   target="_blank"
@@ -163,88 +141,37 @@ const PeopleDetails = () => {
               )}
             </div>
           </div>
+
+          <div className="text-[#E9C46A] flex gap-1 mt-3">
+            <div className="flex gap-2 items-center">
+              <i className="text-[18px] ri-star-fill text-[#E9C46A]"></i>{" "}
+              <p>{Math.floor(info.details.popularity)}</p>
+            </div>
+
+            <span className="text-xl ml-4">•</span>
+            <h1>
+              {info.details.birthday}
+              {info.details.deathday && ` - ${info.details.deathday}`}
+            </h1>
+            <span className="text-xl ml-4">•</span>
+            <h1>{info.details.place_of_birth}</h1>
+          </div>
+
+          <h1 className="mb-3 text-3xl mt-5">Biography</h1>
+          <p className="mb-1 text-[19px]">{info.details.biography}</p>
         </div>
       </div>
 
-      {/*part 3 available platforms/watchproviders*/}
-      {/* <div className="w-[80%] flex flex-col gap-y-5 mt-10">
-        {info.watchProvider && info.watchProvider.flatrate && (
-          <div className="flex gap-x-10 items-center text-white font-semibold">
-            <h1>Watch Now:</h1>
-            {info.watchProvider.flatrate.map((w) => (
-              <img
-                key={w.provider_id}
-                title={w.provider_name}
-                className="w-[5vh] h-[5vh] object-cover rounded-md"
-                src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
-                alt=""
-              />
-            ))}
-          </div>
-        )}
+      {/* Part 5 movies*/}
+      <hr className="border-t border-gray-500 opacity-50 my-4 mx-16 mt-10" />
 
-        {info.watchProvider && info.watchProvider.rent && (
-          <div className="flex gap-x-10 items-center text-white font-semibold">
-            <h1>Rent:</h1>
-            {info.watchProvider.rent.map((w) => (
-              <img
-                key={w.provider_id}
-                title={w.provider_name}
-                className="w-[5vh] h-[5vh] object-cover rounded-md"
-                src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
-                alt=""
-              />
-            ))}
-          </div>
-        )}
-
-        {info.watchProvider && info.watchProvider.buy && (
-          <div className="flex gap-x-10 items-center text-white font-semibold">
-            <h1>Buy:</h1>
-            {info.watchProvider.buy.map((w) => (
-              <img
-                key={w.provider_id}
-                title={w.provider_name}
-                className="w-[5vh] h-[5vh] object-cover rounded-md"
-                src={`https://image.tmdb.org/t/p/original/${w.logo_path}`}
-                alt=""
-              />
-            ))}
-          </div>
-        )}
-      </div> */}
-
-      {/* Part 4 seasons*/}
-      {/* {info.details.seasons.length > 0 && (
-        <div>
-          <hr className="border-t border-gray-500 opacity-50 my-4 mx-16 mt-10" />
-
-          <h1 className="text-3xl font-bold text-white mt-10 pl-5">Seasons</h1>
-          <TrendingCards
-            data={info.details.seasons}
-            showName={info.details.name}
-            seriesId={info.details.id}
-          />
-        </div>
-      )} */}
-
-      {/* Part 5 recommendations and similar*/}
-      {/* <hr className="border-t border-gray-500 opacity-50 my-4 mx-16 mt-10" />
-
-      <h1 className="text-3xl font-bold text-white mt-10 pl-5">
-        {info.recommendations.length > 0
-          ? "Recommendations"
-          : info.similar.length > 0
-          ? "Similar"
-          : ""}
+      <h1 className="text-3xl font-bold text-white mt-10 pl-5 mb-3">
+        {info.movieCredits.cast.length > 0 && `Movies by ${info.details.name}`}
       </h1>
       <TrendingCards
-        data={
-          info.recommendations.length > 0 ? info.recommendations : info.similar
-        }
+        data={info.movieCredits.cast.length > 0 && info.movieCredits.cast}
+        title="movies"
       />
-
-      <Outlet /> */}
     </div>
   ) : (
     <Loading />
