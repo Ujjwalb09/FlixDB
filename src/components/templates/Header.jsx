c719fabfb3501c9a4acd6e9bba5c435a7ddb64b2
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-07-29 16:29:51 +0530

    minor changes to header component

--- src/components/templates/Header.jsx
+++ src/components/templates/Header.jsx
@@ -98,7 +98,7 @@ const Header = ({ data }) => {
             to={`/${
               data.media_type == "movie" ? "movies" : "tv_shows"
             }/details/${data.id}/trailer`}
-            className="mt-5 px-3 py-2 text-sm rounded-full font-semibold bg-[#E9C46A] text-white tracking-tight hover:bg-[#AF9350] duration-200 inline-flex items-center justify-center"
+            className="mt-5 px-3 py-2 text-sm rounded-full font-semibold bg-[#E9C46A] text-white tracking-tight hover:bg-[#AF9350] duration-200 inline-flex items-center justify-center hover:scale-105"
           >
             <i className="ri-play-fill text-lg"></i>
             Play Trailer
