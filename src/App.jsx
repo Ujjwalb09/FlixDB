054976eb82d65bdc4dace75b8800e17666b1fc6b
Ujjwal Bhatt <ujjwalbhatt09@gmail.com>
2024-08-17 20:21:42 +0530

    cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import LocomotiveScroll from "locomotive-scroll";
 import Loading from "./components/Loading";
 import Trending from "./components/Trending";
 import Popular from "./components/Popular";
@@ -18,7 +17,6 @@ import ContactUs from "./components/ContactUs";
 import EpisodeDetails from "./components/EpisodeDetails";
 
 const App = () => {
-  // const locomotiveScroll = new LocomotiveScroll();
   return (
     <div className="bg-[#1F1E24] w-screen h-screen flex">
       <Routes>
